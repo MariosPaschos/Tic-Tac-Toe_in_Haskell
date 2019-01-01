decdf138b8eacbf75dbbb4b4e0c4325c19c20830
MariosPaschos <mariospaschos@gmail.com>
2019-01-01 20:15:35 +0000

    Added functions that check for winner across the board

--- Game.hs
+++ Game.hs
@@ -1,4 +1,5 @@
 module Game where
+
     import Player
 
     getOpponent :: Player -> Player
@@ -7,17 +8,15 @@ module Game where
         | getSign player == O = Player X
         | otherwise = Player Empty
 
-
-    selectSign :: IO Sign  
-    selectSign = do
+    selectPlayer :: IO Sign
+    selectPlayer = do
         putStrLn "Choose a sign to play (X | O)"
         choice <- getChar
         let s = charToSign choice
         if s == X then return X
         else if s == O then return O
-        else selectSign
+        else selectPlayer
         
-
     isBoardFull :: [Sign] -> Bool
     isBoardFull board@(x:xs)
         | (Empty `elem` board) == True = False
@@ -35,11 +34,44 @@ module Game where
 
     displayBoard :: Show a => [a] -> IO () 
     displayBoard board@(x:xs) = do
-        print $ take 3 board
-        print $ take 3 board
-        print $ take 3 board
+        print $ [(board !! n)| n <- [0..2]]
+        print $ [(board !! n)| n <- [3..5]]
+        print $ [(board !! n)| n <- [6..8]]
 
 
+    isRowWin board@(x:xs)
+        | ([board !! n| n <- [0..2]] == (replicate 3 X)) == True = X
+        | ([board !! n| n <- [0..2]] == (replicate 3 O)) == True = O
+        | ([board !! n| n <- [3..5]] == (replicate 3 X)) == True = X
+        | ([board !! n| n <- [3..5]] == (replicate 3 O)) == True = O
+        | ([board !! n| n <- [6..8]] == (replicate 3 X)) == True = X
+        | ([board !! n| n <- [6..8]] == (replicate 3 O)) == True = O
+        | otherwise = Empty
+
+    isColWin board@(x:xs)    
+        | ([board !! n| n <- [0,3,6]] == (replicate 3 X)) == True = X
+        | ([board !! n| n <- [0,3,6]] == (replicate 3 O)) == True = O
+        | ([board !! n| n <- [1,4,7]] == (replicate 3 X)) == True = X
+        | ([board !! n| n <- [1,4,7]] == (replicate 3 O)) == True = O
+        | ([board !! n| n <- [2,5,8]] == (replicate 3 X)) == True = X
+        | ([board !! n| n <- [2,5,8]] == (replicate 3 O)) == True = O
+        | otherwise = Empty
+
+    isDiagWin board@(x:xs)    
+        | ([board !! n| n <- [0,4,8]] == (replicate 3 X)) == True = X
+        | ([board !! n| n <- [0,4,8]] == (replicate 3 O)) == True = O
+        | ([board !! n| n <- [2,4,6]] == (replicate 3 X)) == True = X
+        | ([board !! n| n <- [2,4,6]] == (replicate 3 O)) == True = O
+        | otherwise = Empty
+
+    isWinner board@(x:xs)
+        | isRowWin board == X = X
+        | isRowWin board == O = O
+        | isColWin board == X = X
+        | isColWin board == O = O
+        | isDiagWin board == X = X
+        | isDiagWin board == O = O
+        | otherwise = Empty
 
 
 
